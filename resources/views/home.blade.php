75f3364b77e8f21acb2b3ede79be6a4cdb6070a7
mirzaferdi78@gmail.com <mirzaferdi78@gmail.com>
2023-11-04 23:07:59 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: #e3f6f5;
+        }
+        .card{
+            background-color: #fffffe;
+            color: #272343;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center">Hello World!!!</h1>
+        <div class="card w-1 mb-3 shadow p-3 mb-5  rounded" style="max-width: 540px;">
+            <div class="row g-0 justify-content-center">
+              <div class="col-md-4">
+                <img src="{{url('img/foto.jpg')}}" class="img-fluid rounded" alt="...">
+              </div>
+              <div class="col-md-8 ">
+                <div class="card-body">
+                  <h4 class="card-title">Mochammad Mirza Ferdinand Hakim</h4>
+                  <p class="card-text justify-content-end">Hello, I am a Malang State Polytechnic PSDKU Lumajang student who is currently doing a Fullstack Web Development internship in Arkatama</p>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
